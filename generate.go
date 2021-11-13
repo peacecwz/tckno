5ad0d53e15df3810bf869cfa5c4262e1e1c19761
İbrahim Aydın <info@ibrahimaydin.net>
2021-11-13 17:35:59 +0300

    refactored: rules and variable names at validate and generate functions

--- generate.go
+++ generate.go
@@ -5,10 +5,11 @@ import (
 )
 
 func Generate() string {
-
 	var (
-		digits     [11]int
-		tcIdentity string
+		digits          [11]int
+		tcIdentity      string
+		sumOfEvenDigits int
+		sumOfOddDigits  int
 	)
 
 	digits[0] = Random(1, 10)
@@ -17,17 +18,18 @@ func Generate() string {
 	}
 
 	for i := 0; i < 9; i++ {
-		digits[9] += digits[i]
+		if i%2 == 0 {
+			sumOfEvenDigits += digits[i]
+		} else {
+			sumOfOddDigits += digits[i]
+		}
 	}
 
-	evenTotal := digits[0] + digits[2] + digits[4] + digits[6] + digits[8]
-	oddTotal := digits[1] + digits[3] + digits[5] + digits[7]
+	digits[9] = (sumOfEvenDigits*7 - sumOfOddDigits) % 10
+	digits[10] = (sumOfEvenDigits + sumOfOddDigits + digits[9]) % 10
 
-	digits[9] = (evenTotal*7 - oddTotal) % 10
-	digits[10] = (evenTotal + oddTotal + digits[9]) % 10
-
-	for i := 0; i < len(digits); i++ {
-		tcIdentity += strconv.Itoa(digits[i])
+	for _, digit := range digits {
+		tcIdentity += strconv.Itoa(digit)
 	}
 
 	return tcIdentity
--- validate.go
+++ validate.go
@@ -13,21 +13,16 @@ var (
 )
 
 func Validate(tckNumber string) (valid bool, err error) {
-
 	var (
-		first10NumberSum       int
-		evenNumbersSum         int
-		oddNumbersSum          int
-		n                      int
-		numberBeforeLastNumber int
-		lastNumber             int
+		sumOfEvenDigits  int
+		sumOfOddDigits   int
+		digit            int
+		penultimateDigit int
+		lastDigit        int
 	)
-
-	valid = false
 
 	if len(tckNumber) != 11 {
 		err = errInvalidLength
-
 		return
 	}
 
@@ -36,54 +31,40 @@ func Validate(tckNumber string) (valid bool, err error) {
 		return
 	}
 
-	numberBeforeLastNumber, err = strconv.Atoi(string(tckNumber[9]))
-
+	penultimateDigit, err = strconv.Atoi(string(tckNumber[9]))
 	if err != nil {
 		return
 	}
 
-	lastNumber, err = strconv.Atoi(string(tckNumber[10]))
-
+	lastDigit, err = strconv.Atoi(string(tckNumber[10]))
 	if err != nil {
 		return
 	}
 
-	for i := 0; i < len(tckNumber); i++ {
-
-		n, err = strconv.Atoi(string(tckNumber[i]))
-
+	for i, tckDigit := range tckNumber {
+		digit, err = strconv.Atoi(string(tckDigit))
 		if err != nil {
 			return
 		}
 
-		if i != 10 {
-			first10NumberSum += n
-		}
-
 		if i%2 == 0 && i <= 8 {
-			evenNumbersSum += n
+			sumOfEvenDigits += digit
 		}
 
 		if i%2 != 0 && i < 8 {
-			oddNumbersSum += n
+			sumOfOddDigits += digit
 		}
 	}
 
-	isFirst10NumberSumEqualLastNumber := (first10NumberSum % 10) == lastNumber
-
-	if isFirst10NumberSumEqualLastNumber == false {
-		err = errInvalidSum
+	isPenultimateDigitValid := (sumOfEvenDigits*7-sumOfOddDigits)%10 == penultimateDigit
+	if !isPenultimateDigitValid {
+		err = errInvalidIdentityValidation
 		return
 	}
 
-	isRuleTrue := ((evenNumbersSum*7 + oddNumbersSum*9) % 10) == numberBeforeLastNumber
-	isEvenRuleTrue := (evenNumbersSum*8)%10 == lastNumber
-
-	rule := (evenNumbersSum*7-oddNumbersSum)%10 == numberBeforeLastNumber
-	isLastNumberEven := lastNumber%2 == 0
-
-	if (isRuleTrue && isEvenRuleTrue && rule && isLastNumberEven) == false {
-		err = errInvalidIdentityValidation
+	isLastDigitValid := (sumOfEvenDigits+sumOfOddDigits+penultimateDigit)%10 == lastDigit
+	if !isLastDigitValid {
+		err = errInvalidSum
 		return
 	}
 
